fd079748958e1204a13be0a5411d77408a14603d
Fatih Tanrıverdi <fatih.tanriverdi@dnatech.io>
2022-05-11 15:16:45 +0300

    react-flip-move dependecy deleted.

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,13 +1,12 @@
 import Image from 'next/image'
-import React from 'react'
 
 import { ThumbUpIcon } from '@heroicons/react/outline';
-import { forwardRef } from 'react';
-const Thumbnail = forwardRef(({ result }, ref) => {
+
+const Thumbnail = ({ result }) => {
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
 
     return (
-        <div ref={ref} className='p-2 group cursor-pointer transition duration-200
+        <div className='p-2 group cursor-pointer transition duration-200
          ease-in transform sm:hover:scale-105 hover:z-50'>
             <Image
                 src={`${BASE_URL}${result.backdrop_path || result.poster_path}`
@@ -31,6 +30,6 @@ const Thumbnail = forwardRef(({ result }, ref) => {
             </div>
         </div>
     )
-})
+}
 
 export default Thumbnail
